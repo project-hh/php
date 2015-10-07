4b4e558996607d6d6e0fbca97aa22b2cf50c4ae5
hakhagmon <h@phcm.ru>
2015-10-07 16:13:02 +0300

    Create excel CSV.php

---
+++ excel CSV.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class CSV {
+    //private $_csv_file = null;
+    public $_csv_file = null;
+    public function __construct($csv_file) {
+        if (file_exists($csv_file)) {
+            $this->_csv_file = $csv_file;
+        }
+        else throw new Exception("Файл \"$csv_file\" не найден");
+    }
+    public function getCSV() {
+        $handle = fopen($this->_csv_file, "r");
+        $array_line_full = array();
+        while (($line = fgetcsv($handle, 0, ";")) !== FALSE) {
+            $array_line_full[] = $line;
+        }
+        fclose($handle);
+        return $array_line_full;
+    }
+}
+
+///
+$csv = new CSV("file.csv");
+
+$get_csv = $csv->getCSV();
+foreach ($get_csv as $value)
+{
+echo $value[5];
+}
+
+
+
+?>
