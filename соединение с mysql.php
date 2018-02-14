92d6edcd833b2cc6719007120bf885ee54539bd6
hakhagmon <h@phcm.ru>
2018-02-15 01:30:43 +0300

    Update соединение с mysql.php

--- соединение с mysql.php
+++ соединение с mysql.php
@@ -12,5 +12,12 @@ $data =$result->fetch_assoc();
 $a = $data['id'];
 ....
 
+ $data[1] = $result->fetch_all(MYSQLI_ASSOC);   
+    
+    
+    
+$data = $result->fetch_all(MYSQLI_NUM );
+$count = $result->num_rows;
+    
 $mysqli->close();
 ?>
