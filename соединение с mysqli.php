0eba85ec2574f622a300508eb6c83decec4fa5fd
hakhagmon <h@phcm.ru>
2018-02-21 02:25:09 +0300

    PHP

--- соединение с mysqli.php
+++ соединение с mysqli.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Created by hh.
+ * User: hakhagmon
+ * Date: 21.02.2018
+ * Time: 2:20
+ */
+
 $mysqli = new mysqli('92.14..88', 'username', 'psw', 'bdname');
 if ($mysqli->connect_errno) {
     echo 'Не удалось подключиться к MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
@@ -7,7 +14,7 @@ if ($mysqli->connect_errno) {
 $mysqli->set_charset('utf8');
 
 
-$result = $mysqli->query('SELECT * FROM  table ORDER BY  id DESC LIMIT 0 , 1');
+$result = $mysqli->query('SELECT * FROM  `table` ORDER BY  `id` DESC LIMIT 0 , 1');
 $data = $result->fetch_assoc();
 $a = $data['id'];
 
